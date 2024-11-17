cc84da9c4bc3d90cee89f46af98141ca86961e8e
Renata C R <152975781+crrenatacr@users.noreply.github.com>
2024-11-17 19:58:01 +0100

    RecreatingTheProjectConfiguration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http'; // New method to provide HttpClient
+import { NgModule } from '@angular/core';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +14,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideHttpClient()  // Aqui você substitui o HttpClientModule por provideHttpClient
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
