3cbbc3bd97b879575b35a98a8a333057a06b6fde
Renata C R <152975781+crrenatacr@users.noreply.github.com>
2024-11-17 21:30:00 +0100

    ModuleTsUpdateAndUserRegistrationFormAdd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http'; // New method to provide HttpClient
 import { NgModule } from '@angular/core';
-
-
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserRegistrationFormComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatSnackBarModule,
+    
   ],
   providers: [
-    provideHttpClient()  // Aqui você substitui o HttpClientModule por provideHttpClient
-  ],
+    provideHttpClient(),
+    provideAnimationsAsync()  
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/user-registration-form/user-registration-form.component.cy.ts
@@ -0,0 +1,7 @@
+import { UserRegistrationFormComponent } from './user-registration-form.component'
+
+describe('UserRegistrationFormComponent', () => {
+  it('should mount', () => {
+    cy.mount(UserRegistrationFormComponent)
+  })
+})
