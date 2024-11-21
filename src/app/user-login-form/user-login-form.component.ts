9853ee5cd4864b30eea5967696940f01fdde8b6c
Renata C R <152975781+crrenatacr@users.noreply.github.com>
2024-11-21 20:37:03 +0100

    UserLoginForm

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,4 +1,3 @@
-// src/app/user-registration-form/user-registration-form.component.ts
 import { Component, OnInit, Input } from '@angular/core';
 
 // You'll use this import to close the dialog on success
@@ -10,39 +9,41 @@ import { FetchApiDataService } from '../fetch-api-data.service';
 // This import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
 
-
 @Component({
   selector: 'app-user-login-form',
   templateUrl: './user-login-form.component.html',
-  styleUrls: ['./user-login-form.component.scss']
+  styleUrls: ['./user-login-form.component.scss'],
 })
 export class UserLoginFormComponent implements OnInit {
-
   @Input() loginData = { Username: '', Password: '' };
 
-constructor(
+  constructor(
     public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar) { }
+    public snackBar: MatSnackBar
+  ) {}
 
-ngOnInit(): void {
+  ngOnInit(): void {}
+
+  // This is the function responsible for sending the form inputs to the backend
+  login(): void {
+    this.fetchApiData.userLogin(this.loginData).subscribe({
+      next: (response) => {
+        console.log(response);
+        localStorage.setItem('user', response.user.Username);
+        localStorage.setItem('token', response.token);
+        // Logic for a successful user login goes here!
+        this.dialogRef.close(); // This will close the modal/dialog on success!
+        this.snackBar.open('User logged in!', 'OK', { duration: 2000 });
+      },
+      error: (error) => {
+        // Show error message
+        console.log(error);
+        this.snackBar.open('User login failed!', 'OK', { duration: 2000 });
+      },
+      complete: () => {
+        // Optional: Add logic for completion, if needed
+      },
+    });
+  }
 }
-
-// This is the function responsible for sending the form inputs to the backend
-login(): void {
-  this.fetchApiData.userLogin(this.loginData).subscribe({
-    next: (response) => {
-      // Logic for a successful user login goes here!
-      this.dialogRef.close(); // This will close the modal/dialog on success!
-      console.log(response);
-      this.snackBar.open('User logged in!', 'OK', { duration: 2000 });
-    },
-    error: (error) => {
-      console.log(error);
-      this.snackBar.open(error, 'OK', { duration: 2000 });
-    },
-    complete: () => {
-      // Logic for completion if necessary
-    }
-  });
-}}
